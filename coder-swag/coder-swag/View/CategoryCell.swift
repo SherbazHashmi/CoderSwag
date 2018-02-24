9dd7ee53e8620d9b487e92734f16496a159eef8d
Sherbaz <sherbaz.hashmi@gmail.com>
2018-02-24 17:54:47 +1100

    Added prototype cells

--- coder-swag/coder-swag/View/CategoryCell.swift
+++ coder-swag/coder-swag/View/CategoryCell.swift
@@ -1,3 +1,4 @@
+
 //
 //  CategoryCell.swift
 //  coder-swag
@@ -10,6 +11,11 @@ import UIKit
 
 class CategoryCell: UITableViewCell {
 
+    
+    @IBOutlet weak var categoryImage : UIImageView!
+    @IBOutlet weak var catesgoryTitle : UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
