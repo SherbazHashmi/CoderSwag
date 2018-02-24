e4d0b1e64a487c3fc9bd813b1418e9bfb10672b2
Sherbaz <sherbaz.hashmi@gmail.com>
2018-02-24 17:54:42 +1100

    Added prototype cells

---
+++ coder-swag/coder-swag/View/CategoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Sherbaz Hashmi on 24/2/18.
+//  Copyright © 2018 Sherbaz Hashmi. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
